c68e4e5432278ee5d3605f7c15838bbc18b5f6f8
rajputankit22 <rajputankit22@gmail.com>
2019-11-24 19:08:22 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => (
+  <div>
+    <h1>Hello world!!</h1>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("root"));
